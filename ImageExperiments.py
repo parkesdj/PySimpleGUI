303f98549966c54f3fb1823dedaf94f8911cd35d
parkesdj <parkesdj.dp@gmail.com>
2022-11-30 20:23:13 +0000

    SECOND COMMIT

--- ImageExperiments.py
+++ ImageExperiments.py
@@ -5,17 +5,22 @@ image_folder = 'C:\PySimpleGUI\Demos\images\emojis\\'
 blank = 'blank_stare_112.png'
 happy = 'clap_112.png'
 sad = 'depressed_112.png'
-layout = [[sg.Image(image_folder + blank, key='-EMOJI-')],
-           [sg.Button('Sad', key='-SAD-'),sg.Button('Happy', key='-HAPPY-'), sg.Button('Blank', key='-BLANK')]]
+layout = [[sg.Text('Make Me ...')], [sg.Image(image_folder + blank, key='-EMOJI-')],
+          [sg.Button('Sad', key='-SAD-'), sg.Button('Happy', key='-HAPPY-'), sg.Button('Blank', key='-BLANK-')]]
 
-window = sg.Window('Image Experiments', layout)
+window = sg.Window('Make Me ...', layout, )
+
 while True :
 
     event, values = window.read()
     print(event, values)
     if event == sg.WIN_CLOSED or event == 'Exit' :
         break
-    if event == '-HAPPY-':
+    if event == '-HAPPY-' :
         window['-EMOJI-'].update(image_folder + happy)
-        print('Smile')
 
+    if event == '-SAD-' :
+        window['-EMOJI-'].update(image_folder + sad)
+
+    if event == '-BLANK-' :
+        window['-EMOJI-'].update(image_folder + blank)

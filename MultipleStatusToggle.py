9a3609295d301c1fa417bf275c17d828ce2db1a8
parkesdj <parkesdj.dp@gmail.com>
2022-12-06 21:42:25 +0000

    First Commit

--- MultipleStatusToggle.py
+++ MultipleStatusToggle.py
@@ -1,24 +1,21 @@
 import PySimpleGUI as sg
 
-graph = sg.Graph((120, 120), (0, 0), (120, 120))
+graph = sg.Graph((140, 140), (0, 0), (140, 140))
 
 status = ['green', 'red']
 
 def led(colour) :
     if colour == 'green':
-        return circle('red')
+        return ('red')
     else:
         return circle('green')
 
-def circle(colour):
-    graph.draw_circle((10, 20), radius=5, fill_color=colour, line_width=2)
-    graph.draw_circle((40, 20), radius=5, fill_color=colour, line_width=2)
-    graph.draw_circle((70, 20), radius=5, fill_color=colour, line_width=2)
-    return
-count = 0
+for i in range(20, 170, 50):
+    graph.draw_circle((i, 70), radius=18, fill_color=colour, line_width=2)
+    return= 0
 layout = [[graph], [sg.Button('Main'), sg.Button('Stdb'), sg.Button('Resv')]]
 
-window = sg.Window('Network Status', layout, element_justification='c', finalize=True)
+window = sg.Window('Network Status', layout, element_justification='c', background_color='yellow', finalize=True)
 led('green')
 
 while True:
@@ -27,11 +24,26 @@ while True:
     if event in (sg.WIN_CLOSED, 'Exit'):
         break
 
-    if event == 'Toggle':
+    if event == 'Main':
         count = count + 1
         if count == 2:
             count = 0
-        led(status[count])
+        update_led(20, status[count])
         print(count)
+
+    if event == 'Stdb':
+        count = count + 1
+        if count == 2:
+            count = 0
+        update_led(70, status[count])
+        print(count)
+
+    if event == 'Resv' :
+        count = count + 1
+        if count == 2 :
+            count = 0
+        update_led(120, status[count])
+        print(count)
+
 
 window.close()

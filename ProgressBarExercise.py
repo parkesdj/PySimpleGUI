ec9cf38dd03455c47b58c0c73fcfae29035db35a
parkesdj <parkesdj.dp@gmail.com>
2022-12-08 17:41:09 +0000

    First Commit

---
+++ ProgressBarExercise.py
@@ -0,0 +1,32 @@
+"""
+ProgressBar Element
+
+Exercise 1
+Make a vertical Progress Bar that shows progress increasing using a button
+
+Instructions:
+Your program will display the value of a counter as it counts up to 20
+Create a layout that has 3 elements
+A vertical Progress Bar that is 20 chars (rows) by 30 pixels. The Maximum value for your Progress Bar should be 20
+A Text Element for showing your counter's value
+A Button with the text "Increment"
+When the Increment button is clicked:
+Increment your counter
+Display the current counter value in your Text Element
+Update the Progress Bar using the counter's value
+"""
+import PySimpleGUI as sg
+
+# Create the layout as described in the exercise
+layout = [[]]
+
+window = sg.Window("Progress Bars", layout)
+
+counter = 0
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+    # add your event processing here
+
+window.close()
